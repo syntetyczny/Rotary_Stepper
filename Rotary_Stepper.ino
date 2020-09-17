ecb76ab3b846f164426b6743d6e53cc40016cad2
Paweł Dobrowolski <pawel.j.dobrowolski@gmail.com>
2020-09-17 21:36:18 +0200

    Stepper control added

--- Rotary_Stepper.ino
+++ Rotary_Stepper.ino
@@ -1,9 +1,21 @@
+#include <AccelStepper.h>
+
+//9 pin is DIR, 8 is PUL in M542 Stepper driver
+AccelStepper stepper(1,8,9);
+
 void setup() {
   // put your setup code here, to run once:
-
+  stepper.setMaxSpeed(5000);
+  stepper.setAcceleration(100);
+  stepper.setSpeed(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    do
+    {
+    stepper.runSpeed();
+    }while(millis() < 10000);
+    stepper.setSpeed(0);
 
 }
