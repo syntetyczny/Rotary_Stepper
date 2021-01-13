f774887c662b375b21280c7a3d698c9fc765ece3
Paweł Dobrowolski <pawel.j.dobrowolski@gmail.com>
2021-01-13 20:14:35 +0100

    Added rot encoder menu control

--- Rotary_Stepper.ino
+++ Rotary_Stepper.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-//#include <Encoder.h>
+#include <Encoder.h>
 
 #define BUTTON_ROTARY A3
 #define ENCODER_A A2
@@ -13,28 +13,65 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 AccelStepper stepper(1, 8, 9);
 
 //Rottary encoder
-//Encoder EncoderKnob(ENCODER_A, ENCODER_B);
+Encoder EncoderKnob(ENCODER_A, ENCODER_B);
 
-uint8_t counter = 0;
-uint8_t currentStateCLK;
-uint8_t previousStateCLK;
+int8_t counter = 0;
+int8_t counterPreviousState = 0;
+int8_t currentStateCLK;
+int8_t previousStateCLK;
+
+void screen_1()
+{
+//  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Start");
+  lcd.setCursor(2, 1);
+  lcd.print("Stop");
+  lcd.setCursor(2, 2);
+  lcd.print("Settings");
+  if(counter != counterPreviousState)
+  {
+    lcd.setCursor(0, counter);
+    lcd.print(">");
+    lcd.setCursor(0, counterPreviousState);
+    lcd.print(" ");
+  }
+  else if((counter == 0)&&(counterPreviousState == 0))
+  {
+    lcd.setCursor(0, counter);
+    lcd.print(">");    
+  }
+ 
+}
+
+enum MENU_POS
+{
+ POS_1,
+ POS_2,
+ POS_3,
+ POS_4
+};
+
+void run_motor_countinous()
+{
+  do
+  {
+    stepper.runSpeed();
+  } while (millis() < 10000);
+  stepper.setSpeed(0); 
+}
+
 
 void setup() {
   
-  pinMode (ENCODER_A,INPUT_PULLUP);
-  pinMode (ENCODER_B, INPUT_PULLUP);
-  previousStateCLK = digitalRead(ENCODER_A);
+//  pinMode (ENCODER_A,INPUT_PULLUP);
+//  pinMode (ENCODER_B, INPUT_PULLUP);
+//  previousStateCLK = digitalRead(ENCODER_A);
   
   // put your setup code here, to run once:
   lcd.init();
-  lcd.backlight();
-  lcd.setCursor(1, 0);
-  lcd.print("hello");
-  lcd.setCursor(1, 1);
-  lcd.print("no hell");
-//  lcd.setCursor(1, 2);
-//  lcd.print(EncoderKnob.read());
-
+  lcd.backlight();  
+  screen_1();
   stepper.setMaxSpeed(5000);
   stepper.setAcceleration(100);
   stepper.setSpeed(3000);
@@ -42,13 +79,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  do
-  {
-    stepper.runSpeed();
-  } while (millis() < 10000);
-  stepper.setSpeed(0);
-/*
-  int MenuPosition = EncoderKnob.read() / 4;
+/*  int MenuPosition = EncoderKnob.read() / 4;
   if(MenuPosition < 1)
   {
     EncoderKnob.write(16);
@@ -61,24 +92,27 @@ void loop() {
   }
 */
 
-  currentStateCLK = digitalRead(ENCODER_A);
+  currentStateCLK = EncoderKnob.read()/4 ;
   if(currentStateCLK != previousStateCLK)
   {
-    if(digitalRead(ENCODER_B) != currentStateCLK)
+    if(currentStateCLK < previousStateCLK)
     {
       counter--;
-      lcd.setCursor(1, 2);
-      lcd.clear();
-      lcd.print(counter);   
+      if(counter < 0) counter = 3; 
+//      lcd.clear();
+//      lcd.setCursor(2, 3);
+//      lcd.print(counter);
     }
-    else
+    else if(currentStateCLK > previousStateCLK)
     {
       counter++;
-      lcd.setCursor(1, 2);
-      lcd.clear();
-      lcd.print(counter);
-       
-    }
+      if(counter > 3) counter = 0;
+//      lcd.clear();
+//      lcd.setCursor(2, 3);
+//      lcd.print(counter);
+   }
+    screen_1();
+    counterPreviousState = counter;
   }
   previousStateCLK = currentStateCLK;
 }
