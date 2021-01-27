9f8c67dc93371e0f451592f2290fd05f8c52db77
Paweł Dobrowolski <pawel.j.dobrowolski@gmail.com>
2021-01-27 20:38:35 +0100

    steps settings added

--- Rotary_Stepper.ino
+++ Rotary_Stepper.ino
@@ -10,6 +10,8 @@
 #define LCD_YOFFSET 1
 #define LCD_PS_POSITION 14
 
+#define MOTOR_STEPS 200
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 //9 pin is DIR, 8 is PUL in M542 Stepper driver
@@ -20,20 +22,22 @@ Encoder EncoderKnob(ENCODER_A, ENCODER_B);
 
 uint8_t counter = 0;
 uint8_t continous_percent = 100;
+uint16_t motor_steps = MOTOR_STEPS;
 int8_t counterPreviousState = 0;
 long currentStateCLK = -999;
 long previousStateCLK = -999;
 volatile byte button_state = 0;
 void button_high(void);
+
 void screen_start(void);
 void screen_pos_pointer(void);
+void screen_settings(void);
+void screen_settings_steps(void);
 
 enum MENU_POS
 {
  MAIN,
  MAIN_START,
- MAIN_CONTINOUS,
- MAIN_STEPS,
  MAIN_SETTINGS,
  MAIN_SETTINGS_STEPS,
  MAIN_SETTINGS_ACCELERATION,
@@ -61,7 +65,7 @@ void screen_pos_pointer()
     lcd.setCursor(0, counterPreviousState-LCD_YOFFSET);
     lcd.print(" ");
   }
-  else if((counter == 0)&&(counterPreviousState == 0))
+  else if((counter == 1)&&(counterPreviousState == 1))
   {
     lcd.setCursor(0, counter-LCD_YOFFSET);
     lcd.print(">");    
@@ -69,8 +73,7 @@ void screen_pos_pointer()
 }
 
 void screen_1()
-{
-//  lcd.clear();
+{ 
   lcd.setCursor(2, 0);
   lcd.print("Start   ");
   lcd.setCursor(LCD_PS_POSITION,0);
@@ -98,7 +101,7 @@ void screen_start()
   if(CONTINOUS == motor_mode)
   {
   do{
-    lcd.setCursor(4,3);
+    lcd.setCursor(19,3);
     lcd.print(button_state);
     lcd.setCursor(LCD_PS_POSITION,0);
     current_encoder_state = EncoderKnob.read()/4;
@@ -139,6 +142,65 @@ void run_motor_countinous()
   stepper.setSpeed(0); 
 }
 
+void screen_settings()
+{
+    lcd.setCursor(2, 0);
+    lcd.print("Steps   ");
+    lcd.setCursor(LCD_PS_POSITION,0);
+    lcd.print(motor_steps);
+    lcd.setCursor(LCD_PS_POSITION+3,0);
+    lcd.print(" st");
+ 
+
+
+    lcd.setCursor(2, 1);
+    lcd.print("Acceleration");
+  
+    lcd.setCursor(2, 2);
+    lcd.print("Speed   ");
+    lcd.setCursor(LCD_PS_POSITION,2);
+    lcd.print("1000");
+  
+    lcd.setCursor(2, 3);
+    lcd.print("Mode");
+}
+
+void screen_settings_steps()
+{
+  int encoder_state = EncoderKnob.read();
+  int current_encoder_state = 0;
+  EncoderKnob.write(current_encoder_state);
+  int previous_encoder_state = 0;
+
+  do{
+    lcd.setCursor(19,3);
+    lcd.print(button_state);
+    lcd.setCursor(LCD_PS_POSITION,0);
+    current_encoder_state = EncoderKnob.read()/4;
+    if(current_encoder_state != previous_encoder_state)
+    {
+    if(current_encoder_state > previous_encoder_state)
+    {
+      motor_steps++;
+    }
+    if(current_encoder_state < previous_encoder_state)
+    {
+      motor_steps--;
+    }
+    }
+
+    previous_encoder_state = current_encoder_state;
+    lcd.print("   ");
+    lcd.setCursor(LCD_PS_POSITION,0);
+    lcd.print(motor_steps);
+    lcd.setCursor(18,0);
+    lcd.print("st");
+   
+  }while(LOW == button_state);
+  button_state = LOW;
+  EncoderKnob.write(encoder_state);
+  screen_settings();
+}
 
 void setup() {
   
@@ -159,23 +221,52 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-    lcd.setCursor(4,3);
+    lcd.setCursor(19,3);
     lcd.print(button_state);
 
-    if(HIGH == button_state){
-      button_state = LOW;
+    if(HIGH == button_state)
+    {  
       if(MAIN == menu_status)
       {
+        button_state = LOW;
         switch(counter)
         {
           case MAIN_START:
             screen_start();            
-            break; 
+            break;
+          case MAIN_SETTINGS:
+            screen_settings();
+            menu_status = MAIN_SETTINGS;
+            counter = 1;
+            counterPreviousState = 1;
+            EncoderKnob.write(0);
+            previousStateCLK = 0;
+            lcd.setCursor(0,1);
+            lcd.print(" ");
+           screen_pos_pointer();
+            break;
           default:
             screen_1();
         }        
       }
     }
+    if(HIGH == button_state)
+    {
+      if(MAIN_SETTINGS == menu_status)
+      {
+        button_state = LOW;
+        switch(counter)
+        {
+          case MAIN_SETTINGS_STEPS - MAIN_SETTINGS:
+            screen_settings_steps();
+            break;
+          default:
+            screen_settings();
+        }
+        
+      }
+    }
+    
 
 
 
@@ -186,17 +277,17 @@ void loop() {
     if(currentStateCLK < previousStateCLK)
     {
       counter--;
-      counter %=4 ; 
+      if(counter > 254) counter = 3;  
 
-      lcd.setCursor(2, 3);
+      lcd.setCursor(17, 3);
       lcd.print(counter);
     }
     else if(currentStateCLK > previousStateCLK)
     {
       counter++;
-      counter %= 4;
+      if(counter > 3) counter = 0;
 
-      lcd.setCursor(2, 3);
+      lcd.setCursor(17, 3);
       lcd.print(counter);
     }
   screen_pos_pointer(); 
